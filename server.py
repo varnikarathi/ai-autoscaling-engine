4024384580665e1b797d259d7c402fd3a703b985
varnikarathi <varnikarathi24@gmail.com>
2025-12-15 13:46:32 +0530

    Add request counter to track server load

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-
+from http.server import HTTPServer,BaseHTTPRequestHandler
+request_count=0
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        print("Request received")
+        global request_count
+        request_count+=1
+        print(f"Request_count:{request_count}")
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello! Server is running.")
-
-server = HTTPServer(("localhost", 8000), MyServer)
-
+        self.wfile.write(f"Number of requests recieved:{request_count}".encode())
+server=HTTPServer(("localhost",8000),MyServer)
 print("Server running at http://localhost:8000")
 server.serve_forever()
