dbced42b47f8a8034bc74c1a11618f23e02a6e18
varnikarathi <varnikarathi24@gmail.com>
2025-12-16 08:45:34 +0530

    Filter noisy data

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 from http.server import HTTPServer,BaseHTTPRequestHandler
+import time
 request_count=0
+start_time=time.time()
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         global request_count
+        #to ignore browser noise(favicon.ico)
+        if self.path=="/favicon.ico":
+            self.send_response(204)
+            self.end_headers()
+            return 
         request_count+=1
-        print(f"Request_count:{request_count}")
+        elapsed_time=time.time()-start_time
+        rps=request_count/elapsed_time if elapsed_time>0 else 0
+        print(f"Request_count:{request_count} rps:{rps:.2f}")
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(f"Number of requests recieved:{request_count}".encode())
+        self.wfile.write(f"Number of requests recieved:{request_count}\n Requests per second:{rps:.2f}".encode())
 server=HTTPServer(("localhost",8000),MyServer)
 print("Server running at http://localhost:8000")
 server.serve_forever()
